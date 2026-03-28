a855b329939f53725cac5f57fcb1ccc8f3fb37f9
Alexeyts0Y <tsoyalexey331@gmail.com>
2026-03-28 19:08:55 +0300

    #6 feat: added dockerfile and docker-compose files, modified postgres.go file, fimalized main file

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -1,16 +1,41 @@
 package repository
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"log/slog"
+	"time"
 
-	// Импортируем твой пакет с зашитыми файлами
-	// Замени "твоя_папка_проекта" на имя из твоего go.mod
 	"github.com/Alexeyts0Y/test_task_em/migrations"
+	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
+
+func InitDB(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse dsn: %w", err)
+	}
+
+	config.MaxConns = 10
+	config.MinConns = 2
+	config.MaxConnIdleTime = time.Minute * 5
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping db: %w", err)
+	}
+
+	slog.Info("Database connection established")
+	return pool, nil
+}
 
 func RunMigrations(dbURL string) error {
 	d, err := iofs.New(migrations.FS, ".")
